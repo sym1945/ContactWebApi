62882e977503c7bf2a79f4ae17ff6fdd3afcd4b4
ymshin <sym1945@gmail.com>
2023-06-01 23:04:31 +0900

    EmployeeParser 추가

---
+++ src/ContactWebApi.App/Parsers/EmployeeParser.cs
@@ -0,0 +1,22 @@
+﻿using ContactWebApi.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactWebApi.App.Parsers
+{
+    public class EmployeeParser : IEmployeeParser
+    {
+        public IList<Employee> Parse(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<Employee> Parse(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ContactWebApi.App/Parsers/IEmployeeParser.cs
@@ -0,0 +1,11 @@
+﻿using ContactWebApi.App.Models;
+
+namespace ContactWebApi.App.Parsers
+{
+    public interface IEmployeeParser
+    {
+        IList<Employee> Parse(string text);
+
+        IAsyncEnumerable<Employee> Parse(Stream stream);
+    }
+}
