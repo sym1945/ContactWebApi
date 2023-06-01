846b48a2df5f91c3f21ce44c61e169bc98333ab4
ymshin <sym1945@gmail.com>
2023-06-01 22:27:07 +0900

    EmployeeController 추가

---
+++ src/ContactWebApi.App/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactWebApi.App.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Tel { get; set; } = string.Empty;
+        public DateOnly Joined { get; set; }
+    }
+}
--- src/ContactWebApi/Controllers/EmployeeController.cs
+++ src/ContactWebApi/Controllers/EmployeeController.cs
@@ -1,9 +1,10 @@
+using ContactWebApi.App.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactWebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class EmployeeController : ControllerBase
     {
         private readonly ILogger<EmployeeController> _Logger;
@@ -11,6 +12,24 @@ namespace ContactWebApi.Controllers
         public EmployeeController(ILogger<EmployeeController> logger)
         {
             _Logger = logger;
+        }
+
+        [HttpGet]
+        public Task<List<Employee>> GetEmployees([FromQuery] EmployeeRequest request)
+        {
+            return Task.FromResult(new List<Employee>());
+        }
+
+        [HttpGet("{name}")]
+        public Task<List<Employee>> GetEmployeeDetail(string name)
+        { 
+            return Task.FromResult(new List<Employee>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployees()
+        {
+            return CreatedAtAction(nameof(GetEmployees), null);
         }
     }
 }
